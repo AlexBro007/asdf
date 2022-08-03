4d9015d3bed8acea91b73cc80e5dbfebac75c961
Alex Bro <pataudevochka@gmail.com>
2022-08-03 22:01:10 +0300

    tasks completed

--- pylearn.py
+++ pylearn.py
@@ -13,3 +13,43 @@
 # print("Hello, " + name + " how are you doing?")
 
 
+#Неделя 2. Условный оператор If.
+
+
+#Задание1.jpg. Возраст.
+# age = input("enter your age: ")
+#
+# try:
+# 	age_input = int(age)
+#
+# 	if age_input >= 4 and age_input <= 7:
+# 		print("You have to go to a kindergarten")
+# 	elif age_input > 7 and age_input < 17:
+# 		print("You have to go to a school")
+# 	elif age_input >= 17 and age_input < 22:
+# 		print("You have to go to a university")
+# 	elif age_input >= 22 and age_input <= 59:
+# 		print("You have to work")
+# 	elif age_input >= 60:
+# 		print("You have to retire")
+#
+# except Exception as ex:
+# 	print("use only numbers")
+
+#Задание2.jpg. Сравнение строк.
+def my_func(str1, str2):
+
+    if str1 == str2:
+        return 1
+    elif len(str1) >= len(str2) and not str2 == "learn":
+        return 2
+    elif str2 == "learn" and len(str1) != len(str2) or len(str1) == len(str2):
+        return 3
+    else:
+        print("conditions arent met")
+
+str1 = input()
+str2 = input()
+
+r = my_func(str1, str2)
+print(r)
